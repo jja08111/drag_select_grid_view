7874d74dcb52bbb57c919b9cd8b27341ffa3e212
Minseong Kim <57604817+jja08111@users.noreply.github.com>
2021-01-23 15:26:13 +0900

    Fix typo

--- lib/drag_select_grid_view.dart
+++ lib/drag_select_grid_view.dart
@@ -1,5 +1,5 @@
 library drag_select_grid_view;
 
-export 'src/drag_select_grid_view/drag_select_grid_view.dart'
+export 'src/drag_select_grid_view/drag_select_grid_view.dart';
 export 'src/drag_select_grid_view/drag_select_grid_view_controller.dart';
 export 'src/drag_select_grid_view/selection.dart' hide SelectionManager;
